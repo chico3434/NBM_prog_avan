c0d9f874fd1993d1e01ff382670ebc0419b83d77
chico3434 <chicokarkow@hotmail.com>
2022-02-01 20:00:58 -0300

    Alterados os exemplos e as exibições

--- demo.py
+++ demo.py
@@ -1,22 +1,21 @@
-ham_freq = {'Dear': 8, 'Friend': 5, 'Lunch': 3, 'Money': 1}
+ham_freq = {'relatório': 7, 'reunião': 6, 'promoção': 2, 'desconto': 0}
 
-spam_freq = {'Dear': 2, 'Friend': 1, 'Lunch': 0, 'Money': 4}
+spam_freq = {'relatório': 1, 'reunião': 1, 'promoção': 8, 'desconto': 5}
 
 print('Probabilidades condicionais de não spams')
 for word in ham_freq:
     p = ham_freq.get(word) / sum(ham_freq.values())
-    print(p)
+    print('P(H|{})={}'.format(word, p))
 
 print('Probabilidades condicionais de spams')
 for word in spam_freq:
     p = spam_freq.get(word) / sum(spam_freq.values())
-    print(p)
+    print('P(S|{})={}'.format(word, p))
 
-prior_probability_ham = 8 / (8 + 4)
-prior_probability_spam = 4 / (8 + 4)
+prior_probability_ham = 8 / (8 + 8)
+prior_probability_spam = 8 / (8 + 8)
 
-example_email = ['Dear', 'Friend']
-
+example_email = ['relatório', 'reunião']
 
 def calculate_proportional_probabilities(message):
     proportional_probability_ham = prior_probability_ham
@@ -29,17 +28,24 @@ def calculate_proportional_probabilities(message):
     return proportional_probability_ham, proportional_probability_spam
 
 
-proportional_probability_ham, proportional_probability_spam = calculate_proportional_probabilities(example_email)
+def print_analise(message):
+    proportional_probability_ham, proportional_probability_spam = calculate_proportional_probabilities(message)
+    print(message)
+    print('Probabilidade de não ser spam dado a mensagem é proporcional a: ')
+    print(proportional_probability_ham)
 
-print('Probabilidade de não ser spam dado a mensagem é proporcional a: ')
-print(proportional_probability_ham)
+    print('Probabilidade de ser spam dado a mensagem é proporcional a: ')
+    print(proportional_probability_spam)
+    if(proportional_probability_ham > proportional_probability_spam):
+        print('Provavelmente não é spam!')
+    else:
+        print('Provavelmente é spam!')
 
-print('Probabilidade de ser spam dado a mensagem é proporcional a: ')
-print(proportional_probability_spam)
+print_analise(example_email)
 
-# Probabilidade de 0 em spam por causa de Lunch
-example_email = ['Lunch', 'Money', 'Money', 'Money', 'Money']
-print(calculate_proportional_probabilities(example_email))
+# Probabilidade de 0 em ham por causa de desconto
+example_email = ['desconto', 'promoção', 'promoção', 'promoção', 'promoção']
+print_analise(example_email)
 
 # somar 1 nas frequencias
 for word in ham_freq:
@@ -47,6 +53,5 @@ for word in ham_freq:
 
 for word in spam_freq:
     spam_freq.update({word: spam_freq.get(word)+1})
-print(calculate_proportional_probabilities(['Dear', 'Friend']))
-print(calculate_proportional_probabilities(example_email))
-
+print_analise(['relatório', 'reunião'])
+print_analise(example_email)
